ed9d42fbc00866ffcc835c4c09e7757acace3e2a
florianfmmartin <florian.fm.martin@gmail.com>
2021-08-18 15:29:45 -0400

    maj

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -83,6 +83,34 @@
     };
     neovim = {
       enable = true;
+      plugins = with pkgs.vimPlugins; [
+        {
+          plugin = gruvbox-material;
+	  config = ''
+	    set termguicolors
+	    let g:gruvbox_material_background = 'medium'
+	    colorscheme gruvbox-material
+	  '';
+	}
+	{
+	  plugin = vim-airline;
+	  config = ''
+            let g:airline_powerline_fonts = 1
+            let g:airline_theme = 'lucius'
+            let g:airline_section_error = '''
+            let g:airline_section_warning = '''
+            let g:airline#extensions#tabline#enabled = 1
+            let g:airline#extensions#tabline#fnamemod = ':t'
+            let g:airline#extensions#tabline#buffers_label = '''
+            let g:airline_section_c = '%f'
+            let g:airline_section_z = '%l/%L : %02c'
+            let g:airline#extensions#tabline#ignore_bufadd_pat = '!|defx|gundo|nerd_tree|startify|tagbar|undotree|vimfiler'
+            let g:airline#extensions#term#enabled = 0
+            set noshowmode
+	  '';
+	}
+	vim-airline-themes
+      ];
       viAlias = true;
       vimAlias = true;
       withNodeJs = true;
